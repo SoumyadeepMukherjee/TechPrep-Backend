4893e8d015eda3b5b635bfcbb343651c46430751
Soumyadeep <soumyadeepmukherjee6527@gmail.com>
2023-05-07 12:57:10 +0530

    Exception handling implemented

--- src/main/java/com/examportal/examserver/ExamserverApplication.java
+++ src/main/java/com/examportal/examserver/ExamserverApplication.java
@@ -1,10 +1,14 @@
 package com.examportal.examserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExamserverApplication {
+	
+	private Logger logger=LoggerFactory.getLogger(this.getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamserverApplication.class, args);
--- src/main/java/com/examportal/examserver/entity/Quiz.java
+++ src/main/java/com/examportal/examserver/entity/Quiz.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -32,6 +33,8 @@ public class Quiz {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int qid;
 	private String title;
+	
+	@Column(length=5000)
 	private String description;
 	private String maxMarks;
 	private String noOfQs;
